d1b338d87aa755ad27e98571a75d437e8a77ec50
sukriti136 <106966222+sukriti136@users.noreply.github.com>
2025-02-27 22:03:55 +0530

    added database saving functionality

--- smartOnboard_backend/app/routers/api_calls.py
+++ smartOnboard_backend/app/routers/api_calls.py
@@ -1,8 +1,9 @@
+from fastapi import APIRouter, Body
 from app.services.hello_service import get_hello_message
 from app.services.docx_to_json import docx_to_json
 from app.services.agent_plan import agent_plan
 from app.services.payment_fix import payment_fix
-from fastapi import APIRouter, Body
+from app.services.save_to_db import save_form_data
 
 router = APIRouter()
 
@@ -21,3 +22,7 @@ async def agent_planner(file_path: str = Body(...)):
 @router.post("/paymentFix")
 async def payment_fixer(payment: dict = Body(...)):
     return payment_fix(payment)
+
+@router.post("/saveFormData")
+async def save_form_data_endpoint(data: dict = Body(...)):
+    return save_form_data(data.get('formData'))
