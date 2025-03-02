62c8d3b85a2dc2741fd2536ecd3749ed7972e740
sukriti136 <106966222+sukriti136@users.noreply.github.com>
2025-03-02 22:24:09 +0530

    return saved data reference number

---
+++ smartOnboard_backend/app/services/retrieve_from_db.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+def retrieve_from_db():
+    # Get all the data from the database
+    conn = sqlite3.connect('./formdata.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT id FROM formdata ORDER BY id DESC LIMIT 1")
+    last_entry = cursor.fetchone()
+    conn.close()
+    return last_entry
